555f4f59ed3967cadd361b63f576ddbc0b47614d
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-06-13 15:26:13 +0300

    update abonent return value

--- src/Factory.php
+++ src/Factory.php
@@ -6,14 +6,9 @@ class Factory extends \Sokil\Rest\Client\Factory
 {
     public function updateAbonentProfile(\GoWeb\Api\Model\Client $client)
     {
-        try {
-            $response = $this
-                ->createRequest('UpdateAbonentProfile')
-                ->setClientProfile($client)
-                ->send();
-        } catch (\Exception $e) {
-
-        }
-        
+        return $this
+            ->createRequest('UpdateAbonentProfile')
+            ->setClientProfile($client)
+            ->send();       
     }
 }
