f1bb9c330837bfae6c0f99013095dda3f193c117
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-06-13 15:32:15 +0300

    defien request namespace

--- src/Factory.php
+++ src/Factory.php
@@ -4,6 +4,8 @@ namespace GoWeb\MiddlewareAPI;
 
 class Factory extends \Sokil\Rest\Client\Factory
 {
+    protected $_requestClassNamespace = '\GoWeb\MiddlewareAPI\Request';
+    
     public function updateAbonentProfile(\GoWeb\Api\Model\Client $client)
     {
         return $this
